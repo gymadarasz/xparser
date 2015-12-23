283a6cdbb4b53d384e85b02fa5756859bb42c047
gymadarasz <gyula.madarasz@gmail.com>
2015-12-23 23:46:53 +0000

    autoload and tests

---
+++ tests/XParserTests.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Gyula Madarasz <gyula.madarasz at gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace gymadarasz\xparser\tests;
+
+use gymadarasz\minitest\MiniTestAbstract;
+use gymadarasz\xparser\XNode;
+
+/**
+ * Description of XParserTests
+ *
+ * @author Gyula Madarasz <gyula.madarasz at gmail.com>
+ */
+class XParserTests extends MiniTestAbstract {
+	
+	public function run() {
+		$this->start('mainTest');
+	}
+	
+	public function mainTest() {
+		$tpl = new XNode(
+'<html>
+	<head>
+		<title>Test page</title>
+	</head>
+	<body>
+		<h1>Lorem ipsum</h1>		
+
+		<div />
+
+		<div>asd</div>
+
+		<div id= "hello01" asdasdw />
+<!--
+		<div id= "hello02" asdasdw class="message" asdasd />
+-->
+		<hr>
+
+		<div id="hello1" class="message"> Hello World! </div>
+
+		<hr>
+
+		<div id="hello2" class="message selected"> before <span>Hello World!</span> after </div>
+
+		<hr>
+
+		<div id="hello3" class="message"> before <div>Hello <span>here</span> World!</div> after </div>
+
+		<hr>
+		
+		<input type="text" id="myinput1" value="my value here..">
+
+	</body>
+</html>');
+
+		$before = $tpl->find('div#hello2.selected.message, div#hello1')->inner();
+		$tpl->find('div#hello2.selected.message, div#hello1')->inner('yupeeee!');
+		$after = $tpl->find('div#hello2.selected.message, div#hello1')->inner();
+		$this->equ($before, ' Hello World! ');
+		$this->equ($after, 'yupeeee!');
+
+		$before = $tpl->find('html body input')->attr('value');
+		$tpl->find('input')->attr('value', 'elembe!');
+		$after = $tpl->find('html body input')->attr('value');
+		$this->equ($before, 'my value here..');
+		$this->equ($after, 'elembe!');
+	}
+	
+}
